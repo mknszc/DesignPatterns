2cd454de701fd88c9ebabf900d3980d67aaf9a28
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-01 22:31:10 +0300

    edit style

---
+++ Src/Creational/StaticFactory/Tests/StaticFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DesignPatterns\Src\Creational\StaticFactory\Tests;
+
+namespace DesignPatterns\Src\Creational\StaticFactory\Tests;
+
+use InvalidArgumentException;
+use DesignPatterns\Src\Creational\StaticFactory\FormatNumber;
+use DesignPatterns\Src\Creational\StaticFactory\FormatString;
+use DesignPatterns\Src\Creational\StaticFactory\StaticFactory;
+use PHPUnit\Framework\TestCase;
+
+class StaticFactoryTest extends TestCase
+{
+    public function testCanCreateNumberFormatter()
+    {
+        $this->assertInstanceOf(FormatNumber::class, StaticFactory::factory('number'));
+    }
+
+    public function testCanCreateStringFormatter()
+    {
+        $this->assertInstanceOf(FormatString::class, StaticFactory::factory('string'));
+    }
+
+    public function testException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        StaticFactory::factory('object');
+    }
+}
