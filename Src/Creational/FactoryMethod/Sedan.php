e2bf582f2f94799bd3fa67c03d22e1ceab2418f1
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-04 21:14:33 +0300

    factory methods style edited

--- Src/Creational/FactoryMethod/Sedan.php
+++ Src/Creational/FactoryMethod/Sedan.php
@@ -4,7 +4,6 @@ namespace DesignPatterns\Src\Creational\FactoryMethod;
 
 class Sedan implements Car
 {
-
     public function getType(): string
     {
         return 'Sedan';
--- Src/Creational/FactoryMethod/Suv.php
+++ Src/Creational/FactoryMethod/Suv.php
@@ -4,7 +4,6 @@ namespace DesignPatterns\Src\Creational\FactoryMethod;
 
 class Suv implements Car
 {
-
     public function getType(): string
     {
         return 'Suv';
