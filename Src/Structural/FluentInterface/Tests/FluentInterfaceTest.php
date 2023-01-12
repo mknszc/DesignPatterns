0774d694b09bcdca2fbd3342abf83192dea6cfb6
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-12 22:05:36 +0300

    Fluent Interface design pattern test method

---
+++ Src/Structural/FluentInterface/Tests/FluentInterfaceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DesignPatterns\Src\Structural\FluentInterface\Tests;
+
+use FluentInterfaceTest;
+use PHPUnit\Framework\TestCase;
+
+class FluentInterfaceTest extends TestCase
+{
+
+}
