7e70d98adadf4441bb1c52f396cfcd8744671b47
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-12 22:40:41 +0300

    Fluent Interface design pattern test method

--- Src/Structural/FluentInterface/Tests/FluentInterfaceTest.php
+++ Src/Structural/FluentInterface/Tests/FluentInterfaceTest.php
@@ -2,10 +2,17 @@
 
 namespace DesignPatterns\Src\Structural\FluentInterface\Tests;
 
-use FluentInterfaceTest;
+use DesignPatterns\Src\Structural\FluentInterface\User;
 use PHPUnit\Framework\TestCase;
 
 class FluentInterfaceTest extends TestCase
 {
+    public function testBuildUser()
+    {
+        $user = (new User())
+            ->setUserName('Özcan')
+            ->setWorkTitle('Software Developer');
 
+        $this->assertSame('User name: Özcan, Work title: Software Developer', (string) $user);
+    }
 }
