e5bffbb5e6be9327ae15f173737a5c9806ffc67e
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-19 22:04:29 +0300

    Dependency injection

---
+++ Src/Structural/DependencyInjection/DatabaseConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DesignPatterns\Src\Structural\DependencyInjection;
+
+class DatabaseConnection
+{
+    public function __construct(private DatabaseConfiguration $configuration)
+    {
+    }
+
+    public function getDsn(): string
+    {
+        return sprintf(
+            '%s:%s@%s:%d',
+            $this->configuration->getUsername(),
+            $this->configuration->getPassword(),
+            $this->configuration->getHost(),
+            $this->configuration->getPort()
+        );
+    }
+}
