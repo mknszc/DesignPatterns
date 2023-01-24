a6116f68b2ef9a80a3923467decc6c2d3c669a94
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-01-24 21:38:14 +0300

    Abstract Factory

---
+++ Src/Creational/AbstractFactory/WinCsvWriter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DesignPatterns\Src\Creational\AbstractFactory;
+
+class WinCsvWriter implements CsvWriter
+{
+    public function write(array $line): string
+    {
+        return join(',', $line) . "\r\n";
+    }
+}
