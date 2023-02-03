d728f670be9499a13489842f46e0faf5ce1c9b46
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2023-02-03 21:04:41 +0300

    Facade design pattern

--- Src/Structural/Registry/Registry.php
+++ Src/Structural/Registry/Registry.php
@@ -2,7 +2,33 @@
 
 namespace DesignPatterns\Src\Structural\Registry;
 
-class Registry
-{
+use InvalidArgumentException;
 
+abstract class Registry
+{
+    public const LOGGER = 'logger';
+
+    private static array $services = [];
+
+    private static array $allowedKeys = [
+        self::LOGGER,
+    ];
+
+    final public static function set(string $key, Service $value): void
+    {
+        if (!in_array($key, self::$allowedKeys)) {
+            throw new InvalidArgumentException('Invalid key given');
+        }
+
+        self::$services[$key] = $value;
+    }
+
+    final public static function get(string $key): Service
+    {
+        if (!in_array($key, self::$allowedKeys) || !isset(self::$services[$key])) {
+            throw new InvalidArgumentException('Invalid key given');
+        }
+
+        return self::$services[$key];
+    }
 }
